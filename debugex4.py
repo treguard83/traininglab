ec74f04a0b9523cc34c677aebcbf7f9e04fda8a6
Paul-Grubb <Paul.Grubb@nationwide.co.uk>
2021-07-13 16:25:47 +0100

    Test on list length now dynamic

--- debugex4.py
+++ debugex4.py
@@ -5,7 +5,7 @@ item_list = ["Burger", "Hotdog", "Bun", "Ketchup", "Cheese"]
 n = 0
 i = 0
 
-while n < 5:
+while n < len(item_list)-1:
     for item_name in item_list:
         print("Item " + str(n+1) + " is " + item_name)
         n = n + 1
