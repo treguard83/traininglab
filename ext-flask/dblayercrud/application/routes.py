c843475171e99ef6a4eb9c27eef8543d4e785ebb
Paul-Grubb <Paul.Grubb@nationwide.co.uk>
2021-07-21 15:22:12 +0100

    flask crud stuff done

--- ext-flask/dblayercrud/application/routes.py
+++ ext-flask/dblayercrud/application/routes.py
@@ -1,6 +1,11 @@
 from application import app, db
 from application.models import Games
 
+@app.route('/')
+@app.route('/home')
+def home():
+    return 'This is the home page'
+
 @app.route('/add')
 def add():
     new_game = Games(name="New Game")
