e0f20b63804397421536c6b9a2e020abc3e0ef72
Paul-Grubb <Paul.Grubb@nationwide.co.uk>
2021-07-23 11:19:15 +0100

    Fixed date and decimal fields

--- ext-flask/forms/app.py
+++ ext-flask/forms/app.py
@@ -1,17 +1,48 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, IntegerField, DecimalField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+from datetime import date
+import decimal
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 
+class BetterDecimalField(DecimalField):
+    """
+    Very similar to WTForms DecimalField, except with the option of rounding
+    the data always.
+    """
+    def __init__(self, label=None, validators=None, places=2, rounding=None,
+                 round_always=False, **kwargs):
+        super(BetterDecimalField, self).__init__(
+            label=label, validators=validators, places=places, rounding=
+            rounding, **kwargs)
+        self.round_always = round_always
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            try:
+                self.data = decimal.Decimal(valuelist[0])
+                if self.round_always and hasattr(self.data, 'quantize'):
+                    exp = decimal.Decimal('.1') ** self.places
+                    if self.rounding is None:
+                        quantized = self.data.quantize(exp)
+                    else:
+                        quantized = self.data.quantize(
+                            exp, rounding=self.rounding)
+                    self.data = quantized
+            except (decimal.InvalidOperation, ValueError):
+                self.data = None
+                raise ValueError(self.gettext('Not a valid decimal value'))
+
 class BasicForm(FlaskForm):
     first_name = StringField('First Name')
     last_name = StringField('Last Name')
-    dobbo = DateField(format='%Y-%m-%d') # opt arg format='%Y-%m-%d'
+    dobbo = DateField('Date of Birth',format='%Y-%m-%d', validators=[DataRequired()], default=date.today) # opt arg format='%Y-%m-%d'
     nummo = IntegerField()
-    decca = DecimalField(places=2, rounding=None)
+    decca = BetterDecimalField(round_always=True)#DecimalField(places=2, rounding=None)
     selecta = SelectField('Programming Language', choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')])
     submit = SubmitField('Add Data')
 
