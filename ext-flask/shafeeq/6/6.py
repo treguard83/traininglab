5ea7d59b8974fc6b379cb9acfc0d3bb52325a831
Paul-Grubb <Paul.Grubb@nationwide.co.uk>
2021-08-04 16:56:57 +0100

    Sixth flask exercise sorted! BOOM!

--- ext-flask/shafeeq/6/6.py
+++ ext-flask/shafeeq/6/6.py
@@ -2,8 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/<tt>")
+@app.route("/")
+def homejames():
+	return render_template("index.htm")
+
+@app.route("/timestable/<tt>")
 def index(tt):
 	return render_template("timestable.htm",T=int(tt))
+
+@app.route("/abc",methods=['GET','POST'])
+def index2():
+	return "Hello my friends from GET and POST"
+
+@app.route("/abc",methods=['DELETE'])
+def index4():
+	return "Hello my friends from DELETE"
 
 app.run(debug=True)
