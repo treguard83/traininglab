aaddf861eac62b507b75cc31d597be1f711141c6
Paul-Grubb <Paul.Grubb@nationwide.co.uk>
2021-08-04 14:41:15 +0100

    Fourth exercise with Shafeeq done

--- ext-flask/shafeeq/4/4.py
+++ ext-flask/shafeeq/4/4.py
@@ -14,12 +14,15 @@ def aboutus():
 def servos():
 	return render_template("services.htm")
 
-@app.route("/trainers/shafeeq")
-def trainer1():
-	return render_template("trainers.htm",name="Shafeeq",address="London")
-
-@app.route("/trainers/paul")
-def trainer2():
-	return render_template("trainers.htm",name="Paul",address="Swindon")
+@app.route("/trainers/<tname>/<add>")
+def trainer1(tname,add):
+	color1 = "orange"
+	if add == "London":
+		color1 = "red"
+	if add == "Swindon":
+		color1 = "blue"
+	if add == "Cardiff":
+		color1 = "blue"
+	return render_template("trainers.htm",name=tname,address=add,color=color1)
 
 app.run(debug=True)
